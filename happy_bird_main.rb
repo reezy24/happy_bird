b22f7ccd56bf681eb9592e55da0d8c9eae590b8e
reez <zreye24@gmail.com>
2019-09-03 14:14:27 +1000

    add test script

--- happy_bird_main.rb
+++ happy_bird_main.rb
@@ -1,28 +1,49 @@
 # graphics
 pipe_head = "[|||||]"
 pipe_body = " ||||| "
+pipe_gap =  "       "
+
 happy_bird = ":)"
 sad_bird = ":("
-ground = "_\n/"
-test_line = "                   [||||]                                "
-distance_between_pipes = 30
-pipe_gap = 6
-min_pipe_height = 2
-game_height = 16
-game_speed = 0.05 # move the screen five times a second
 
-def update(screen_height)
-    screen_height.times do
-        puts "|====8"
+# game vars
+game_height = 16
+distance_between_pipes = 30
+pipe_gap_size = 6
+min_pipe_height = 2
+max_pipe_height = game_height - pipe_gap_size - min_pipe_height
+game_speed = 0.05 # move the screen five times a second
+game_running = true
+pipes_on_screen = 4
+x_shift = 0
+pipes = []
+
+#modify pipe graphics based on distance_between_pipes
+pipe_head = pipe_head.center(pipe_head.length + distance_between_pipes / 2, " ")
+pipe_body = pipe_body.center(pipe_body.length + distance_between_pipes / 2, " ")
+pipe_gap  = pipe_gap .center(pipe_gap .length + distance_between_pipes / 2, " ")
+
+def update
+    # draw the screen
+    system("clear")
+    game_height.times do |i| # each row
+        pipe_heights.each do |height|
+            # current height is equal to top or bottom pipe height - head
+            # current height is between top or bottom pipe - gap
+            # else - body
+            if i == top_pipe_head or i == bottom_pipe_head
+                
+            # wait
+        end
+        sleep(game_speed)
     end
 end
 
-
-while test_line.length > 0
-    system("clear")
-    p(test_line)
-    chars = test_line.chars
-    chars.shift
-    test_line = chars.join
-    sleep(game_speed)
+def game_start
+    game_running = true
+    while game_running
+        update
+    end
 end
+
+#game_running
---
+++ happy_bird_test.rb
@@ -0,0 +1,7 @@
+require "test/unit"
+require_relative "happy_bird_main"
+
+class HappyBirdTest < Test::Unit:TestCase
+    
+end
+
