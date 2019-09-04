38751acbda154755de4de69906d966b05297372e
reez <zreye24@gmail.com>
2019-09-05 09:12:30 +1000

    add draw_to_screen function

--- happy_bird_main.rb
+++ happy_bird_main.rb
@@ -3,6 +3,8 @@ require "tty-reader"
 require "curses"
 require_relative "bird"
 
+#include Curses
+
 SETTINGS = {
     
     #config
@@ -10,7 +12,7 @@ SETTINGS = {
     DIST_BETWEEN_PIPES_X: 25,
     DIST_BETWEEN_PIPES_Y: 6,
     MIN_PIPE_HEIGHT: 2,
-    SCROLL_SPEED: 0.05, # 1/SCROLL_SPEED = how many times the screen moves a second
+    SCROLL_SPEED: 0.05,
     STARTER_PIPE_HEIGHT: 5,
 
     #graphics
@@ -22,24 +24,38 @@ SETTINGS = {
 
 }
 
-def update(settings, pipes, pipe_offset, bird)
+# bird code to be placed in function
+# bird_y = bird.y_pos.round
+# bird_y.times do |i|
+#     if i == bird_y
+#         Curses.addstr(":)")
+#     else
+#         Curses.addstr("\n")
+#     end
+# end
+# Curses.addstr(bird.y_pos.to_s) # debug
+
+def draw_to_screen(screen, x, y, str)
+    row = screen[y]
+    row[x..str.length] = str
+end
+
+def draw_pipes(settings, pipes, pipe_offset, screen)
     s = settings
-    # draw the screen
-    Curses.clear
     range = pipe_offset..(s[:DIST_BETWEEN_PIPES_X] * (pipes.length - 1) + pipe_offset)
-    # s[:SCREEN_HEIGHT].times do |i| # each row
-    #     this_row = ""
-    #     pipes.each do |pipe|
-    #         if !pipe || (i > pipe.top_height && i < pipe.bottom_head) # no pipe exists - used for spacing out the start
-    #             this_row += s[:PIPE_GAP].center(s[:DIST_BETWEEN_PIPES_X], " ")
-    #         elsif i == pipe.top_height || i == pipe.bottom_head # render pipe head
-    #             this_row += s[:PIPE_HEAD].center(s[:DIST_BETWEEN_PIPES_X], " ")
-    #         else # render pipe body
-    #             this_row += s[:PIPE_BODY].center(s[:DIST_BETWEEN_PIPES_X], " ")
-    #         end
-    #     end
-    #     Curses.addstr(this_row[range]+"\n")
-    # end
+    s[:SCREEN_HEIGHT].times do |i| # each row
+        this_row = ""
+        pipes.each do |pipe|
+            if !pipe || (i > pipe.top_height && i < pipe.bottom_head) # no pipe exists - used for spacing out the start
+                this_row += s[:PIPE_GAP].center(s[:DIST_BETWEEN_PIPES_X], " ")
+            elsif i == pipe.top_height || i == pipe.bottom_head # render pipe head
+                this_row += s[:PIPE_HEAD].center(s[:DIST_BETWEEN_PIPES_X], " ")
+            else # render pipe body
+                this_row += s[:PIPE_BODY].center(s[:DIST_BETWEEN_PIPES_X], " ")
+            end
+        end
+        Curses.addstr(this_row[range]+"\n")
+    end
     bird_y = bird.y_pos.round
     bird_y.times do |i|
         if i == bird_y
@@ -48,22 +64,50 @@ def update(settings, pipes, pipe_offset, bird)
             Curses.addstr("\n")
         end
     end
-    #Curses.addstr(bird.y_pos.to_s)
-    # 
+    Curses.refresh
+end
+
+# duplicate of draw_pipes atm
+def render(screen)
+    s = settings
+    Curses.clear
+    range = pipe_offset..(s[:DIST_BETWEEN_PIPES_X] * (pipes.length - 1) + pipe_offset)
+    s[:SCREEN_HEIGHT].times do |i| # each row
+        this_row = ""
+        pipes.each do |pipe|
+            if !pipe || (i > pipe.top_height && i < pipe.bottom_head) # no pipe exists - used for spacing out the start
+                this_row += s[:PIPE_GAP].center(s[:DIST_BETWEEN_PIPES_X], " ")
+            elsif i == pipe.top_height || i == pipe.bottom_head # render pipe head
+                this_row += s[:PIPE_HEAD].center(s[:DIST_BETWEEN_PIPES_X], " ")
+            else # render pipe body
+                this_row += s[:PIPE_BODY].center(s[:DIST_BETWEEN_PIPES_X], " ")
+            end
+        end
+        Curses.addstr(this_row[range]+"\n")
+    end
+    bird_y = bird.y_pos.round
+    bird_y.times do |i|
+        if i == bird_y
+            Curses.addstr(":)")
+        else
+            Curses.addstr("\n")
+        end
+    end
     Curses.refresh
 end
 
 def game_start(settings)
 
-    # curses testing
-    Curses.init_screen
-    Curses.noecho
+    Curses.init_screen # prevent flicker
+    Curses.noecho # hide user input
+
+    s = settings
 
     # game state vars
-    s = settings
     game_running = false
     x_offset = 1
     reader = TTY::Reader.new
+    screen = []
 
     # initialise pipes
     starter_pipe = Pipe.new(s[:STARTER_PIPE_HEIGHT], s[:SCREEN_HEIGHT], s[:DIST_BETWEEN_PIPES_Y])
