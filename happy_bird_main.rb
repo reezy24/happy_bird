8e2f44d3b5334d274cffa0ded1e277fcc8d3a282
reez <zreye24@gmail.com>
2019-09-05 09:34:26 +1000

    refactor, flicker issues returned

--- happy_bird_main.rb
+++ happy_bird_main.rb
@@ -35,9 +35,9 @@ SETTINGS = {
 # end
 # Curses.addstr(bird.y_pos.to_s) # debug
 
-def draw_to_screen(screen, x, y, str)
-    row = screen[y]
-    row[x..str.length] = str
+def draw_to_screen(screen, y, x, str)
+    screen[y] = "" if !screen[y] # create row
+    screen[y][x..str.length] = str
 end
 
 def draw_pipes(settings, pipes, pipe_offset, screen)
@@ -54,44 +54,14 @@ def draw_pipes(settings, pipes, pipe_offset, screen)
                 this_row += s[:PIPE_BODY].center(s[:DIST_BETWEEN_PIPES_X], " ")
             end
         end
-        Curses.addstr(this_row[range]+"\n")
+        draw_to_screen(screen, i, 0, this_row[range]+"\n")
     end
-    bird_y = bird.y_pos.round
-    bird_y.times do |i|
-        if i == bird_y
-            Curses.addstr(":)")
-        else
-            Curses.addstr("\n")
-        end
-    end
-    Curses.refresh
 end
 
-# duplicate of draw_pipes atm
 def render(screen)
-    s = settings
     Curses.clear
-    range = pipe_offset..(s[:DIST_BETWEEN_PIPES_X] * (pipes.length - 1) + pipe_offset)
-    s[:SCREEN_HEIGHT].times do |i| # each row
-        this_row = ""
-        pipes.each do |pipe|
-            if !pipe || (i > pipe.top_height && i < pipe.bottom_head) # no pipe exists - used for spacing out the start
-                this_row += s[:PIPE_GAP].center(s[:DIST_BETWEEN_PIPES_X], " ")
-            elsif i == pipe.top_height || i == pipe.bottom_head # render pipe head
-                this_row += s[:PIPE_HEAD].center(s[:DIST_BETWEEN_PIPES_X], " ")
-            else # render pipe body
-                this_row += s[:PIPE_BODY].center(s[:DIST_BETWEEN_PIPES_X], " ")
-            end
-        end
-        Curses.addstr(this_row[range]+"\n")
-    end
-    bird_y = bird.y_pos.round
-    bird_y.times do |i|
-        if i == bird_y
-            Curses.addstr(":)")
-        else
-            Curses.addstr("\n")
-        end
+    screen.each do |row|
+        Curses.addstr(row)
     end
     Curses.refresh
 end
@@ -105,7 +75,7 @@ def game_start(settings)
 
     # game state vars
     game_running = false
-    x_offset = 1
+    x_offset = 0
     reader = TTY::Reader.new
     screen = []
 
@@ -117,26 +87,43 @@ def game_start(settings)
     # initialise bird
     bird = Bird.new(0, 5, 0, -20)
 
-    # draw screen but don't start moving yet
-    update(s, pipes, 0, bird)
+    # initialise screen
+    draw_pipes(s, pipes, x_offset, screen)
+    render(screen)
+    
+    # initialise bird
+    # 
+
+    # start on spacebar press
     until reader.read_char == " "
       game_running = false
     end
     game_running = true
 
+    # render every frame until end
     while game_running
-        bird.jump if bird.y_pos >= 10
-        bird.move(50, 0.05)
-        update(s, pipes, x_offset, bird)
-        # delay loop by game_speed
-        sleep(0.05)
+
+        # bird testing
+        #bird.jump if bird.y_pos >= 10
+        #bird.move(50, 0.05)
+        
+        # adjust x_offset
         if x_offset == s[:DIST_BETWEEN_PIPES_X]
             x_offset = 0
             pipes.push(RandomPipe.new(s[:SCREEN_HEIGHT], s[:DIST_BETWEEN_PIPES_Y], s[:MIN_PIPE_HEIGHT])).shift
         else
             x_offset += 1
         end
-        #reader.read_char
+
+        draw_pipes(s, pipes, x_offset, screen)
+
+        # draw bird
+        #
+
+        render(screen)
+
+        sleep(0.05)
+
     end
 end
 
